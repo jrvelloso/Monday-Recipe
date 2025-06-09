09b7d059f0731b23fd69a4b350a3808f3b1fb3a7
jaldeonfilho <jaldeonfilho@hotmail.com>
2025-06-09 11:26:26 +0100

    User vification != null

--- Frontend/recipe-app/src/app/comment/comment.component.ts
+++ Frontend/recipe-app/src/app/comment/comment.component.ts
@@ -1,87 +1,58 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { IComment } from 'src/interfaces/icomment';
 import { CommentService } from 'src/services/comment.service';
-
 
 @Component({
   selector: 'app-comment',
   templateUrl: './comment.component.html',
   styleUrls: ['./comment.component.css']
 })
-export class CommentComponent implements OnInit {
+export class CommentComponent implements OnInit, OnChanges {
 
-  form!: FormGroup;
-  comments!: IComment[];
-  selected: IComment | null = null;
+  @Input() recipeId: number | null = null;
+  comments: IComment[] = [];
+  isLoggedIn: boolean = false;
+  newComment: string = '';
 
-  constructor(private service: CommentService, private fb: FormBuilder) {
-    this.form = this.fb.group({
-      comments: ['', Validators.required],
-      userId: ['', Validators.required],
-      recipeId: ['', Validators.required],
-      isActive: [false]
-    });
-  }
+  constructor(private service: CommentService) {}
 
-  ngOnInit(): void {
-    this.fetch();
-  }
-
-  fetch(): void {
-    this.service.getAll().subscribe((data: IComment[]) => {
-      this.comments = data;
-    });
-  }
-
-  select(item: IComment): void {
-    this.selected = { ...item };
-    this.form.patchValue({
-      comments: item.comments,
-      userId: item.userId,
-      recipeId: item.recipeId,
-      isActive: item.isActive
-    });
-  }
-
-  create(item: IComment): void {
-    this.service.create(item).subscribe(() => {
-      this.fetch();
-      this.clearForm();
-    });
-  }
-
-  update(): void {
-    if (this.selected) {
-      this.service.update(this.selected.id, this.selected).subscribe(() => {
-        this.fetch();
-        this.clearForm();
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['recipeId'] && this.recipeId !== null) {
+      this.service.getByRecipeId(this.recipeId).subscribe(data => {
+        this.comments = data;
       });
     }
   }
 
-  delete(id: number): void {
-    this.service.delete(id).subscribe(() => {
-      this.fetch();
-    });
-  }
+  ngOnInit(): void {
+    // TODO: Replace with actual auth check
+    this.isLoggedIn = !!localStorage.getItem('userToken');
 
-  onSubmit() {
-    if (this.form.valid) {
-      if (this.selected) {
-        this.selected.comments = this.form.value.comments;
-        this.selected.userId = this.form.value.userId;
-        this.selected.recipeId = this.form.value.recipeId;
-        this.selected.isActive = this.form.value.isActive;
-        this.update();
-      } else {
-        this.create(this.form.value);
-      }
+    if (this.recipeId !== null) {
+      this.service.getByRecipeId(this.recipeId).subscribe(data => {
+        this.comments = data;
+      });
     }
   }
 
-  clearForm() {
-    this.form.reset();
-    this.selected = null;
+  submitComment(): void {
+    if (!this.newComment.trim()) return;
+
+    // TODO: Replace with actual user id from auth
+    const userId = 1;
+
+    const comment: Partial<IComment> = {
+      comments: this.newComment,
+      userId: userId,
+      recipeId: this.recipeId!,
+      isActive: true
+    };
+
+    this.service.create(comment as IComment).subscribe(() => {
+      this.newComment = '';
+      this.service.getByRecipeId(this.recipeId!).subscribe(data => {
+        this.comments = data;
+      });
+    });
   }
 }
--- Frontend/recipe-app/src/interfaces/icomment.ts
+++ Frontend/recipe-app/src/interfaces/icomment.ts
@@ -1,7 +1,12 @@
 export interface IComment {
   id: number;
-  isActive: boolean;
   comments: string;
   userId: number;
   recipeId: number;
+  isActive: boolean;
+  user?: {
+    username: string;
+    avatarUrl?: string;
+  };
+  createdAt?: string;
 }
